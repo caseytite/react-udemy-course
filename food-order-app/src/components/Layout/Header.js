346747fa52bd797673e4996519a3d16a03718f42
Casey <caseyjtite@gmail.com>
2022-04-12 13:13:59 -0700

    update header button

---
+++ food-order-app/src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src="./images/meals.jpeg" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
