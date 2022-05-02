6bd1088304fcb2c38de68c370c7cb2c5569bb34e
Casey <caseyjtite@gmail.com>
2022-05-01 23:08:40 -0700

    add effect to cart button

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,34 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = ({ showCartHandler }) => {
   const context = useContext(CartContext);
-  const numOfCartItems = context.items.reduce((acc, cur) => {
+  const [btnBump, setBtnBump] = useState(false);
+  const { items } = context;
+  const numOfCartItems = items.reduce((acc, cur) => {
     return acc + cur.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnBump ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnBump(true);
+    const timer = setTimeout(() => {
+      setBtnBump(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={showCartHandler}>
+    <button className={btnClasses} onClick={showCartHandler}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
