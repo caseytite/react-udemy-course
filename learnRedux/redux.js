3ad9a963ca70cf3e917d3adba7c13373100f9a43
Casey <caseyjtite@gmail.com>
2022-05-14 23:35:08 -0700

    basic redux setup

--- learnRedux/redux.js
+++ learnRedux/redux.js
@@ -1,5 +1,31 @@
-import { configureStore } from "@reduxjs/toolkit";
+const redux = require("redux");
 
-const storeReducer = (prevState, action) => {};
+//reducer function that will update state
+const counterReducer = (currentState = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: ++currentState.counter,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: --currentState.counter,
+    };
+  }
+  return currentState;
+};
 
-const STORE = configureStore({});
+// initializes the store
+const STORE = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  // gets the state from the reducer subscribed to STORE
+  const latestState = STORE.getState();
+  console.log(latestState);
+};
+
+// wires up the subscribe function to the store
+STORE.subscribe(counterSubscriber);
+
+STORE.dispatch({ type: "increment" });
+STORE.dispatch({ type: "decrement" });
