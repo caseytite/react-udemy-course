5ed5bebbbc8fd48fb9217ea556d1bd6a7e560700
Casey <caseyjtite@gmail.com>
2022-05-15 20:46:33 -0700

    use redux for login credentials app wide

--- learnRedux/reactRedux/src/components/Auth.js
+++ learnRedux/reactRedux/src/components/Auth.js
@@ -1,19 +1,50 @@
-import classes from './Auth.module.css';
+import { useState } from "react";
+import classes from "./Auth.module.css";
+import { authActions } from "../store/index";
+import { useDispatch } from "react-redux";
 
 const Auth = () => {
+  const dispatch = useDispatch();
+  // const auth = useSelector((state) => state.auth.isAuthenticated);
+
+  const handleAuthenticate = (e, credentials) => {
+    e.preventDefault();
+    dispatch(authActions.login(credentials));
+  };
+  const [state, setState] = useState({ email: "", password: "" });
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit={(e) => handleAuthenticate(e, state)}>
           <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <label htmlFor="email">Email</label>
+            <input
+              value={state.email}
+              onChange={(e) => {
+                setState((prev) => ({
+                  ...prev,
+                  email: e.target.value,
+                }));
+              }}
+              type="email"
+              id="email"
+            />
           </div>
           <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <label htmlFor="password">Password</label>
+            <input
+              value={state.password}
+              onChange={(e) => {
+                setState((prev) => ({
+                  ...prev,
+                  password: e.target.value,
+                }));
+              }}
+              type="password"
+              id="password"
+            />
           </div>
-          <button>Login</button>
+          <button type="submit">Login</button>
         </form>
       </section>
     </main>
--- learnRedux/reactRedux/src/components/Header.js
+++ learnRedux/reactRedux/src/components/Header.js
@@ -1,19 +1,27 @@
-import classes from './Header.module.css';
+import classes from "./Header.module.css";
+import { authActions } from "../store/index";
+import { useSelector, useDispatch } from "react-redux";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth.isAuthenticated);
+  const handleLogout = () => {
+    dispatch(authActions.logout());
+  };
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
         <ul>
           <li>
-            <a href='/'>My Products</a>
+            <a href="/">My Products</a>
           </li>
           <li>
-            <a href='/'>My Sales</a>
+            <a href="/">My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            {auth && <button onClick={handleLogout}>Logout</button>}
+            {!auth && <button onClick={handleLogout}>Login</button>}
           </li>
         </ul>
       </nav>
